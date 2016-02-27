d1255e5d592db30e4f507975e232f4b3c1f72bdf
dguedo <devin.guedo@gmail.com>
2016-02-27 10:40:45 -0600

    Working

--- server.R
+++ server.R
@@ -7,20 +7,15 @@ shinyServer(function(input, output) {
   # Fill in the spot we created for a plot
   output$samplePlot <- renderPlot({
 
-    rdata <- rnorm(input$n_simulations)
-    n_size <- 40
-    n_simulations = 1:1000
+    # first find the mean for a set of random exponentials
+    # repeat {n_simulations} times
+    means <- data.frame(x=sapply(1:input$n_simulations,function(x) {mean(rexp(input$n_size,0.2))})) 
     
-    # first find the mean for a set of 40 random exponentials, 
-    # repeat 1000 times
-    means <- data.frame(x=sapply(n_simulations,function(x) {mean(rexp(n_size,0.2))})) 
-    
-        
     # histogram
     hist(means$x, breaks=as.numeric(input$n_breaks), prob=T, main="Normal distribution of sample means", xlab="")
 
     # density
-    #lines(density(rdata))
+    lines(density(means$x))
 
   })
 })
--- ui.R
+++ ui.R
@@ -9,28 +9,26 @@ shinyUI(
     
     # Give the page a title
     titlePanel("Random generation for the normal distribution"),
-    h4("This example highlights the capibilities of Histograms and Density Plots"),
+    h4("This example highlights the capabilities of Histograms and Density Plots, by repeatedly taking the mean of a set of random normals and plotting the results."),
    
     # Generate a row with a sidebar
     sidebarLayout(      
       # Define the sidebar with two inputs
       sidebarPanel(
-        helpText("Choose the number of random exponentials per set, and the number of simulations to run."),
+        helpText("Choose the number of random normals per set, the number of simulations to run, and the histogram bin size."),
         hr(),
-
-        selectInput("n_breaks", "Breaks:", 
-                    choices = c(20, 40, 60),
-                    selected = 40),
-        
-        br(),
-        
+        selectInput("n_size", "Set size:", 
+                    choices = c(10, 20, 30, 40),
+                    selected = 20),
         sliderInput("n_simulations", 
                     "Number of observations:", 
                     value = 500,
                     min = 10, 
-                    max = 1000)
-
-        
+                    max = 1000),
+        br(),
+        selectInput("n_breaks", "Size of bins:", 
+                    choices = c(20, 40, 60),
+                    selected = 40)
       ),
 
       # Create a spot for the histogram 
